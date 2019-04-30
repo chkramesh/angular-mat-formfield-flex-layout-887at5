4e78fa24de7be0871e172d52f7b4b2b0ec903059
chkramesh <chkramesh@gmail.com>
2019-04-30 14:43:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-formfield-flex-layout-887at5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-formfield-flex-layout-887at5)
---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { CustomMaterialModule } from './custom-material.module';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, ReactiveFormsModule, CustomMaterialModule, FlexLayoutModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
